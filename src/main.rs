65ebe54160aa9b1b272033176a7b854d0344d05c
Herbert <34774005+Herbert-Karl@users.noreply.github.com>
2019-05-16 22:26:26 +0200

    problem 30

--- src/main.rs
+++ src/main.rs
@@ -22,6 +22,7 @@ fn main() {
     println!("The longest collatz sequence for positive integers smaller {} has {} ", 1_000_000, longest_collatz_sequence(1_000_000));
     //println!("The sum of the digits in {}! (factorial) is {}.", 100, factorial_digit_sum(100));
     println!("The sum of all amicable numbers below {} is {}", 10000, amicable_numbers(10000));
+    println!("The sum of all numbers, which can be written as the sum of fifth powers of their digits, is {}.", digit_fifth_power());
 }
 
 /// problem 1
@@ -198,4 +199,23 @@ fn amicable_numbers(n: u128) -> u128 {
         }
     }
     amicable_nums.into_iter().sum()
+}
+
+/// problem 30
+///
+/// computes the sum of all numbers that can be written as the sum of fifth powers of their digits.
+///
+/// the upper bound for this computation has to be derived:
+/// * as 9 is the biggest digit, 9^5 is our biggest summand
+/// * we need the upper limit of digits, so that a sum of its digit to the fifth power still creates a number with the same amount of digits.
+/// This is 6, as 7*9^5 = 413343, which is not a 7 digit number, so 7 digit numbers cant be written as the sum of the fifth power of their digits
+fn digit_fifth_power() -> u32 {
+    let upper_bond = 6*9_u32.pow(5);
+    let mut vector = Vec::new();
+    for i in 2..upper_bond+1 {
+        if i == i.to_string().chars().map(|x| x.to_digit(10).expect("this is no digit!").pow(5)).sum() {
+            vector.push(i);
+        }
+    }
+    vector.into_iter().sum()
 }
